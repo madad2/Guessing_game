e0bfecf5ad6f43a80b643309d4e8a26b11d61d18
madad <alexander.laputko@ya.ru>
2024-01-29 23:13:04 +0300

    Added dataBinding to fragment_game.xml.

--- app/src/main/java/hfa/madad/guessinggame/GameFragment.kt
+++ app/src/main/java/hfa/madad/guessinggame/GameFragment.kt
@@ -23,17 +23,8 @@ class GameFragment : Fragment() {
         val view = binding.root
         viewModel = ViewModelProvider(this).get(GameViewModel::class.java)
 
-        viewModel.incorrectGuesses.observe(viewLifecycleOwner, Observer { newValue ->
-            binding.incorrectGuesses.text = "Incorrect guesses: $newValue"
-        })
-
-        viewModel.livesLeft.observe(viewLifecycleOwner, Observer { newValue ->
-            binding.lives.text = "You have $newValue lives left"
-        })
-
-        viewModel.secretWordDisplay.observe(viewLifecycleOwner, Observer { newValue ->
-            binding.word.text = newValue
-        })
+        binding.gameViewModel = viewModel
+        binding.lifecycleOwner = viewLifecycleOwner
 
         viewModel.gameOver.observe(viewLifecycleOwner, Observer { newValue ->
             if (newValue) {
