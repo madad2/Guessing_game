45a4cfbf6d28cd48a3b68dfffce8d37a81e3af2d
madad <alexander.laputko@ya.ru>
2024-01-29 18:22:58 +0300

    Updated ResultFragment.kt with viewModelFactory and viewModel.

--- app/src/main/java/hfa/madad/guessinggame/ResultFragment.kt
+++ app/src/main/java/hfa/madad/guessinggame/ResultFragment.kt
@@ -5,12 +5,16 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.get
 import androidx.navigation.findNavController
 import hfa.madad.guessinggame.databinding.FragmentResultBinding
 
 class ResultFragment : Fragment() {
     private var _binding: FragmentResultBinding? = null
     private val binding get() = _binding!!
+    lateinit var viewModel: ResultViewModel
+    lateinit var viewModelFactory: ResultViewModelFactory
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
@@ -18,7 +22,11 @@ class ResultFragment : Fragment() {
         _binding = FragmentResultBinding.inflate(inflater, container, false)
         val view = binding.root
 
-        binding.wonLost.text = ResultFragmentArgs.fromBundle(requireArguments()).result
+        val result = ResultFragmentArgs.fromBundle(requireArguments()).result
+        viewModelFactory = ResultViewModelFactory(result)
+        viewModel = ViewModelProvider(this, viewModelFactory).get(ResultViewModel::class.java)
+
+        binding.wonLost.text = viewModel.result
 
         binding.newGameButton.setOnClickListener {
             view.findNavController()
---
+++ app/src/main/java/hfa/madad/guessinggame/ResultViewModel.kt
@@ -0,0 +1,7 @@
+package hfa.madad.guessinggame
+
+import androidx.lifecycle.ViewModel
+
+class ResultViewModel(finalResult: String) : ViewModel() {
+    val result = finalResult
+}
