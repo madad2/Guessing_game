16c5fa4e8ba83beadcd2419f3b6f699353769699
madad <alexander.laputko@ya.ru>
2024-01-29 23:21:32 +0300

    Added new method in GameViewModel and using dataBinding for calling.

--- app/src/main/java/hfa/madad/guessinggame/GameViewModel.kt
+++ app/src/main/java/hfa/madad/guessinggame/GameViewModel.kt
@@ -63,4 +63,8 @@ class GameViewModel : ViewModel() {
         message += " The word was $secretWord"
         return message
     }
+
+    fun finishGame() {
+        _gameOver.value = true
+    }
 }
